a19c0a42c0148a24a6a125681ec17875bd394553
Roberto Diaz <robertodiaztnf@gmail.com>
2024-05-20 20:12:12 +0200

    zshrc

--- install.sh
+++ install.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-cat bashrc.additions >> ~/.bashrc
-
-cp ./.gitmessage ~
 cp ./.gitconfig ~
 
-# powerline fonts for zsh agnoster theme
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
-cd .. && rm -rf fonts
+# # powerline fonts for zsh agnoster theme
+# git clone https://github.com/powerline/fonts.git
+# cd fonts
+# ./install.sh
+# cd .. && rm -rf fonts
 
 # oh-my-zsh & plugins
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
